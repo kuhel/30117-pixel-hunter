9c9a39d2f8c0453446d0d179af038c7c8d2c3e29
gvorontsov <gleb.vorontsov@datart.com>
2016-12-03 23:19:06 +0300

    test environment were added

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ const mqpacker = require('css-mqpacker');
 const minify = require('gulp-csso');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
+const mocha = require('gulp-mocha');
+require('babel-register');
 
 gulp.task('style', function () {
   gulp.src('sass/style.scss')
@@ -56,6 +58,14 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('test', function () {
+  return gulp
+    .src(['js/**/*.test.js'], { read: false })
+    .pipe(mocha({
+      compilers: {
+        js: 'babel-register'
+      },
+      reporter: 'spec'
+    }));
 });
 
 gulp.task('imagemin', ['copy'], function () {
