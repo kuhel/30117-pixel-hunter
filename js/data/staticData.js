da11daa5d91dc8568209861a5046cf9766413a19
gvorontsov <gleb.vorontsov@datart.com>
2016-12-23 18:38:30 +0300

    useless consts

--- js/data/staticData.js
+++ js/data/staticData.js
@@ -2,19 +2,6 @@
  * Created by glebvorontsov on 30/11/16.
  */
 
-export const Result = {
-  CORRECT: 'correct',
-  WRONG: 'wrong',
-  FAST: 'fast',
-  SLOW: 'slow'
-};
-
-export const QuestionType = {
-  TWO_OF_TWO: 'two-of-two',
-  TINDER_LIKE: 'tinder-like',
-  ONE_OF_THREE: 'one-of-three'
-};
-
 export const AnswerType = {
   PAINTING: 'painting',
   PHOTO: 'photo'
