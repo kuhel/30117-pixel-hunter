0bbab9cad25e51c2685755a622935f3b422a8673
gvorontsov <gleb.vorontsov@datart.com>
2016-12-26 15:34:58 +0300

    hotfixes

--- js/data/staticData.js
+++ js/data/staticData.js
@@ -2,6 +2,12 @@
  * Created by glebvorontsov on 30/11/16.
  */
 
+export const QuestionType = {		
+  TWO_OF_TWO: 'two-of-two',		
+  TINDER_LIKE: 'tinder-like',		
+  ONE_OF_THREE: 'one-of-three'		
+};
+
 export const AnswerType = {
   PAINTING: 'painting',
   PHOTO: 'photo'
